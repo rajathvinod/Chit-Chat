d6f5457d3f77aac6b427a37b80fb88b7e0bf23af
Rajath Vinod <rajathvinod122@gmail.com>
2026-08-21 00:46:26 +0530

    prod changes

--- backend/server.js
+++ backend/server.js
@@ -30,9 +30,9 @@ const __dirname1 = path.resolve();
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname1, "/frontend/build")));
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
-  );
+  app.get("/{*splat}", (req, res) => {
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
+  });
 } else {
   app.get("/", (req, res) => {
     res.send("API is running..");
@@ -49,7 +49,7 @@ const PORT = process.env.PORT;
 
 const server = app.listen(
   PORT,
-  console.log(`Server running on PORT ${PORT}...`.yellow.bold)
+  console.log(`Server running on PORT ${PORT}...`.yellow.bold),
 );
 
 const io = require("socket.io")(server, {
